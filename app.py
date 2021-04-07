219c5af80adc4b1f26c628161df1f0774f41eb6f
Suvid Datta <suvid.datta@gmail.com>
2021-04-07 21:21:55 +0530

    first!

---
+++ app.py
@@ -0,0 +1,46 @@
+# app.py
+from flask import *
+from flask_autoindex import AutoIndex
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = "<PATH>"
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+
+app = Flask(__name__)
+AutoIndex(app, browse_root=UPLOAD_FOLDER)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect('/')
+
+    return '''  
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ forms.py
@@ -0,0 +1,4 @@
+from flask_wtf import FlaskForm
+from wtforms import TextField, IntegerField, TextAreaField, SubmitField, RadioField, SelectField
+from wtforms import validators, ValidationError
+
